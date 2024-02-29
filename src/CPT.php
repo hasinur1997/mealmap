cd69147621d4991bda357d9108b639f968a3de6c
Hasinur Rahman <hasinurrahman3@gmail.com>
2024-02-29 16:35:44 +0600

    update: Update code formating

--- src/CPT.php
+++ src/CPT.php
@@ -14,13 +14,13 @@ class CPT {
 
     /**
      * Loop over the $custom_post_type array and
-     * 
+     *
      * Registers custom post types.
      *
      * @return  void
      */
     public static function register(): void {
-        foreach( self::$custom_post_types as $post_type ) {
+        foreach ( self::$custom_post_types as $post_type ) {
             if ( class_exists( $post_type ) && is_subclass_of( $post_type, CustomPostType::class ) ) {
                 new $post_type();
             }
