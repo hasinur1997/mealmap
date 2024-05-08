0c0c17ac46d847fec40dfff791e1055dffd3616e
Hasinur Rahman <hasinurrahman3@gmail.com>
2024-05-08 07:25:37 +0600

    new: Added stat component

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '4e29c741990e877b0a21');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '9d1860572d0eee4878a6');
